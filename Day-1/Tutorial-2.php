ba1a69941ff4181c71e34d1c577aca77eabddfd2
Kavindu Bimsara <kavidubim2gamaethige@gmail.com>
2025-08-05 21:19:21 +0530

    init tt2

---
+++ Day-1/Tutorial-2.php
@@ -0,0 +1,28 @@
+<?php
+
+$number=60;
+$text="Hello, World!";
+$floatNumber=3.14;
+
+
+$list=array("apple", "banana", "cherry");
+
+echo "The number is:".$number."<br>";
+// echo "<script>alert('This number is - $number');</script>";
+echo "<br>";
+echo "$text";
+echo "<br>";
+echo "<br>";
+echo "$floatNumber";
+echo "<br>";
+// echo "$list";
+var_dump($list[0]);
+echo "<br>";
+var_dump($list[1]);
+echo "<br>";
+var_dump($list[2]);
+echo "<br>";
+
+
+
+?>
